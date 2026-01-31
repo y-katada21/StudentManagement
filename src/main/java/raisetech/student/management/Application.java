ed6bcfa5705d7df3520cdc6fb55beae8aa2b8a31
y-katada21 <l2520956737as@gmail.com>
2026-01-31 14:32:03 +0900

    syokaikomitto

---
+++ src/main/java/raisetech/student/management/Application.java
@@ -0,0 +1,17 @@
+package raisetech.student.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+@GetMapping("hello")
+  public String hello() {
+    return "Hello, World";
+  }
+}
